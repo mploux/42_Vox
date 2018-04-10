e92b6ad4d86d85b8cf0cfb356fab605c7e4483e6
mploux <mploux@student.42.fr>
2018-04-10 19:32:13 +0200

    Added the correct modules

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main()
+int main(void)
 {
-    std::cout << "Hello Vox !" << std::endl;
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
